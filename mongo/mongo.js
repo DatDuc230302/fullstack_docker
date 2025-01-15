27d38ad0f53f0a464e553b1abc13ad0c2efcc726
DucDat2303 <tranducdat2303@gmail.com>
2025-01-15 09:56:40 +0700

    fix

--- mongo/mongo.js
+++ mongo/mongo.js
@@ -1,5 +1,5 @@
-const database = "storage";
-const collectionName = "users";
+const database = 'storage';
+const collectionName = 'users';
 
 // Create a new database.
 use(database);
@@ -8,14 +8,14 @@ use(database);
 const collection = db.getCollection(collectionName);
 
 var data = [
-  {
-    _id: ObjectId("6785f9fc9bfc167b5232d6ae"),
-    displayName: "Trần Đức Đạt",
-    age: 15,
-    createdAt: new Date("2025-01-14T05:45:32.863Z"),
-    updatedAt: new Date("2025-01-14T05:45:32.863Z"),
-    __v: 0,
-  },
+    {
+        _id: ObjectId('6785f9fc9bfc167b5232d6ae'),
+        displayName: 'Trần Đức Đạt',
+        age: 15,
+        createdAt: new Date('2025-01-14T05:45:32.863Z'),
+        updatedAt: new Date('2025-01-14T05:45:32.863Z'),
+        __v: 0,
+    },
 ];
 
 collection.insertMany(data); // Nhập dữ liệu vào collection "users"
